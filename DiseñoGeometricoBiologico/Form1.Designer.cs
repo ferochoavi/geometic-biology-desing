d6a2bb561e0da3382ce8a2a16dc25b09b5631eda
Fernando Ochoa <56469077+ferochoavi@users.noreply.github.com>
2020-05-21 05:11:44 -0500

    We basically did the whole app
    Thank you to Isaac Hernandez who was my partner in the development

--- DiseñoGeometricoBiologico/Form1.Designer.cs
+++ DiseñoGeometricoBiologico/Form1.Designer.cs
@@ -28,108 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.bunifuFlatButton1 = new Bunifu.Framework.UI.BunifuFlatButton();
-            this.bunifuThinButton21 = new Bunifu.Framework.UI.BunifuThinButton2();
-            this.bunifuTileButton1 = new Bunifu.Framework.UI.BunifuTileButton();
+            this.cameraBox = new Emgu.CV.UI.ImageBox();
+            this.startButton = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.saveButton = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.nameLabel = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.nameTextBox = new Bunifu.Framework.UI.BunifuTextbox();
+            ((System.ComponentModel.ISupportInitialize)(this.cameraBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // bunifuFlatButton1
+            // cameraBox
             // 
-            this.bunifuFlatButton1.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
-            this.bunifuFlatButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
-            this.bunifuFlatButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.bunifuFlatButton1.BorderRadius = 0;
-            this.bunifuFlatButton1.ButtonText = "bunifuFlatButton1";
-            this.bunifuFlatButton1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.bunifuFlatButton1.DisabledColor = System.Drawing.Color.Gray;
-            this.bunifuFlatButton1.Iconcolor = System.Drawing.Color.Transparent;
-            this.bunifuFlatButton1.Iconimage = ((System.Drawing.Image)(resources.GetObject("bunifuFlatButton1.Iconimage")));
-            this.bunifuFlatButton1.Iconimage_right = null;
-            this.bunifuFlatButton1.Iconimage_right_Selected = null;
-            this.bunifuFlatButton1.Iconimage_Selected = null;
-            this.bunifuFlatButton1.IconMarginLeft = 0;
-            this.bunifuFlatButton1.IconMarginRight = 0;
-            this.bunifuFlatButton1.IconRightVisible = true;
-            this.bunifuFlatButton1.IconRightZoom = 0D;
-            this.bunifuFlatButton1.IconVisible = true;
-            this.bunifuFlatButton1.IconZoom = 90D;
-            this.bunifuFlatButton1.IsTab = false;
-            this.bunifuFlatButton1.Location = new System.Drawing.Point(322, 43);
-            this.bunifuFlatButton1.Name = "bunifuFlatButton1";
-            this.bunifuFlatButton1.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
-            this.bunifuFlatButton1.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(129)))), ((int)(((byte)(77)))));
-            this.bunifuFlatButton1.OnHoverTextColor = System.Drawing.Color.White;
-            this.bunifuFlatButton1.selected = false;
-            this.bunifuFlatButton1.Size = new System.Drawing.Size(241, 48);
-            this.bunifuFlatButton1.TabIndex = 0;
-            this.bunifuFlatButton1.Text = "bunifuFlatButton1";
-            this.bunifuFlatButton1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.bunifuFlatButton1.Textcolor = System.Drawing.Color.White;
-            this.bunifuFlatButton1.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cameraBox.Location = new System.Drawing.Point(12, 12);
+            this.cameraBox.Name = "cameraBox";
+            this.cameraBox.Size = new System.Drawing.Size(358, 322);
+            this.cameraBox.TabIndex = 2;
+            this.cameraBox.TabStop = false;
             // 
-            // bunifuThinButton21
+            // startButton
             // 
-            this.bunifuThinButton21.ActiveBorderThickness = 1;
-            this.bunifuThinButton21.ActiveCornerRadius = 20;
-            this.bunifuThinButton21.ActiveFillColor = System.Drawing.Color.SeaGreen;
-            this.bunifuThinButton21.ActiveForecolor = System.Drawing.Color.White;
-            this.bunifuThinButton21.ActiveLineColor = System.Drawing.Color.SeaGreen;
-            this.bunifuThinButton21.BackColor = System.Drawing.SystemColors.Control;
-            this.bunifuThinButton21.ButtonText = "ThinButton";
-            this.bunifuThinButton21.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.bunifuThinButton21.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.bunifuThinButton21.ForeColor = System.Drawing.Color.SeaGreen;
-            this.bunifuThinButton21.IdleBorderThickness = 1;
-            this.bunifuThinButton21.IdleCornerRadius = 20;
-            this.bunifuThinButton21.IdleFillColor = System.Drawing.Color.White;
-            this.bunifuThinButton21.IdleForecolor = System.Drawing.Color.SeaGreen;
-            this.bunifuThinButton21.IdleLineColor = System.Drawing.Color.SeaGreen;
-            this.bunifuThinButton21.Location = new System.Drawing.Point(293, 190);
-            this.bunifuThinButton21.Margin = new System.Windows.Forms.Padding(5);
-            this.bunifuThinButton21.Name = "bunifuThinButton21";
-            this.bunifuThinButton21.Size = new System.Drawing.Size(181, 41);
-            this.bunifuThinButton21.TabIndex = 1;
-            this.bunifuThinButton21.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.startButton.ActiveBorderThickness = 1;
+            this.startButton.ActiveCornerRadius = 20;
+            this.startButton.ActiveFillColor = System.Drawing.Color.SeaGreen;
+            this.startButton.ActiveForecolor = System.Drawing.Color.White;
+            this.startButton.ActiveLineColor = System.Drawing.Color.SeaGreen;
+            this.startButton.BackColor = System.Drawing.SystemColors.Control;
+            this.startButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("startButton.BackgroundImage")));
+            this.startButton.ButtonText = "Iniciar Detección y Reconocimiento ";
+            this.startButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.startButton.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.ForeColor = System.Drawing.Color.SeaGreen;
+            this.startButton.IdleBorderThickness = 1;
+            this.startButton.IdleCornerRadius = 20;
+            this.startButton.IdleFillColor = System.Drawing.Color.White;
+            this.startButton.IdleForecolor = System.Drawing.Color.SeaGreen;
+            this.startButton.IdleLineColor = System.Drawing.Color.SeaGreen;
+            this.startButton.Location = new System.Drawing.Point(378, 12);
+            this.startButton.Margin = new System.Windows.Forms.Padding(5);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(289, 86);
+            this.startButton.TabIndex = 3;
+            this.startButton.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
             // 
-            // bunifuTileButton1
+            // saveButton
             // 
-            this.bunifuTileButton1.BackColor = System.Drawing.Color.SeaGreen;
-            this.bunifuTileButton1.color = System.Drawing.Color.SeaGreen;
-            this.bunifuTileButton1.colorActive = System.Drawing.Color.MediumSeaGreen;
-            this.bunifuTileButton1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.bunifuTileButton1.Font = new System.Drawing.Font("Century Gothic", 15.75F);
-            this.bunifuTileButton1.ForeColor = System.Drawing.Color.White;
-            this.bunifuTileButton1.Image = ((System.Drawing.Image)(resources.GetObject("bunifuTileButton1.Image")));
-            this.bunifuTileButton1.ImagePosition = 20;
-            this.bunifuTileButton1.ImageZoom = 50;
-            this.bunifuTileButton1.LabelPosition = 41;
-            this.bunifuTileButton1.LabelText = "Tile 1";
-            this.bunifuTileButton1.Location = new System.Drawing.Point(59, 66);
-            this.bunifuTileButton1.Margin = new System.Windows.Forms.Padding(6);
-            this.bunifuTileButton1.Name = "bunifuTileButton1";
-            this.bunifuTileButton1.Size = new System.Drawing.Size(128, 129);
-            this.bunifuTileButton1.TabIndex = 2;
+            this.saveButton.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.saveButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.saveButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.saveButton.BorderRadius = 0;
+            this.saveButton.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.saveButton.ButtonText = "Guardar Rostro";
+            this.saveButton.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.saveButton.DisabledColor = System.Drawing.Color.Gray;
+            this.saveButton.Iconcolor = System.Drawing.Color.Transparent;
+            this.saveButton.Iconimage = ((System.Drawing.Image)(resources.GetObject("saveButton.Iconimage")));
+            this.saveButton.Iconimage_right = null;
+            this.saveButton.Iconimage_right_Selected = null;
+            this.saveButton.Iconimage_Selected = null;
+            this.saveButton.IconMarginLeft = 0;
+            this.saveButton.IconMarginRight = 0;
+            this.saveButton.IconRightVisible = true;
+            this.saveButton.IconRightZoom = 0D;
+            this.saveButton.IconVisible = true;
+            this.saveButton.IconZoom = 90D;
+            this.saveButton.IsTab = false;
+            this.saveButton.Location = new System.Drawing.Point(376, 282);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.saveButton.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(129)))), ((int)(((byte)(77)))));
+            this.saveButton.OnHoverTextColor = System.Drawing.Color.White;
+            this.saveButton.selected = false;
+            this.saveButton.Size = new System.Drawing.Size(302, 52);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "Guardar Rostro";
+            this.saveButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.saveButton.Textcolor = System.Drawing.Color.White;
+            this.saveButton.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.Location = new System.Drawing.Point(378, 185);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(73, 21);
+            this.nameLabel.TabIndex = 5;
+            this.nameLabel.Text = "Nombre";
+            this.nameLabel.Click += new System.EventHandler(this.nameLabel_Click);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.BackColor = System.Drawing.Color.Silver;
+            this.nameTextBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("nameTextBox.BackgroundImage")));
+            this.nameTextBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.nameTextBox.ForeColor = System.Drawing.Color.SeaGreen;
+            this.nameTextBox.Icon = ((System.Drawing.Image)(resources.GetObject("nameTextBox.Icon")));
+            this.nameTextBox.Location = new System.Drawing.Point(457, 173);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(210, 42);
+            this.nameTextBox.TabIndex = 8;
+            this.nameTextBox.text = "Escribe tu nombre aquí";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(634, 461);
-            this.Controls.Add(this.bunifuTileButton1);
-            this.Controls.Add(this.bunifuThinButton21);
-            this.Controls.Add(this.bunifuFlatButton1);
+            this.ClientSize = new System.Drawing.Size(681, 346);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.cameraBox);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.cameraBox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private Bunifu.Framework.UI.BunifuFlatButton bunifuFlatButton1;
-        private Bunifu.Framework.UI.BunifuThinButton2 bunifuThinButton21;
-        private Bunifu.Framework.UI.BunifuTileButton bunifuTileButton1;
+        private Emgu.CV.UI.ImageBox cameraBox;
+        private Bunifu.Framework.UI.BunifuThinButton2 startButton;
+        private Bunifu.Framework.UI.BunifuFlatButton saveButton;
+        private Bunifu.Framework.UI.BunifuCustomLabel nameLabel;
+        private Bunifu.Framework.UI.BunifuTextbox nameTextBox;
     }
 }
 
--- DiseñoGeometricoBiologico/Form1.cs
+++ DiseñoGeometricoBiologico/Form1.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using Microsoft.VisualBasic.ApplicationServices;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +16,109 @@ namespace DiseñoGeometricoBiologico
 {
     public partial class Form1 : Form
     {
+        // Declare variables
+        MCvFont font = new MCvFont(Emgu.CV.CvEnum.FONT.CV_FONT_HERSHEY_TRIPLEX, 0.6d, 0.6d);
+        HaarCascade faceDetected;
+        Image<Bgr, Byte> Frame;
+        Capture camera;
+        Image<Gray, byte> result;
+        Image<Gray, byte> TrainedFace = null;
+        Image<Gray, byte> grayFace = null;
+        List<Image<Gray, byte>> trainingImages = new List<Image<Gray, byte>>();
+        List<string> labels = new List<string>();
+        List<string> Users = new List<string>();
+        int Count, NumLabels, t;
+        string name, names = null;
+
+        private void nameLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            Count = Count + 1;
+            grayFace = camera.QueryGrayFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            MCvAvgComp[][] DetectedFaces = grayFace.DetectHaarCascade(faceDetected, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size (20, 20));
+            
+            foreach (MCvAvgComp f in DetectedFaces[0])
+            {
+                TrainedFace = Frame.Copy(f.rect).Convert<Gray, byte>();
+                break;
+            }
+            
+            TrainedFace = result.Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            trainingImages.Add(TrainedFace);
+            labels.Add(nameTextBox.text);
+            File.WriteAllText(Application.StartupPath + "/Faces/Faces.txt", trainingImages.ToArray().Length.ToString() + ",");
+            
+            for (int i = 1; i < trainingImages.ToArray().Length + 1; i++)
+            {
+                trainingImages.ToArray()[i - 1].Save(Application.StartupPath + "/Faces/face" + i + ".bmp");
+                File.AppendAllText(Application.StartupPath + "/Faces/Faces.txt", labels.ToArray()[i - 1] + ",");
+            }
+            MessageBox.Show(nameTextBox.text + "Added successfully");
+        }
+
         public Form1()
         {
             InitializeComponent();
+            // Haarcascade is for face detection    
+            faceDetected = new HaarCascade("haarcascade_frontalface_default.xml");
+
+            try
+            {
+                string Labelsinf = File.ReadAllText(Application.StartupPath + "/Faces/Faces.txt");
+                string[] Labels = Labelsinf.Split(',');
+                //El primer label será el del numero de veces de caras guardadas
+                NumLabels = Convert.ToInt16(Labels[0]);
+                Count = NumLabels;
+                string FacesLoad;
+                
+                for (int i = 1; i < NumLabels + 1; i++)
+                {
+                    FacesLoad = "face" + i + ".bmp";
+                    trainingImages.Add(new Image<Gray, byte>(Application.StartupPath + "/Faces/Faces.txt"));
+                    labels.Add(Labels[i]);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Nada en la Base de Datos Weon");
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            camera = new Capture();
+            camera.QueryFrame();
+            Application.Idle += new EventHandler(FrameProcedure);
+        }
+
+        private void FrameProcedure(object sender, EventArgs e)
+        {
+            Users.Add("");
+            Frame = camera.QueryFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            grayFace = Frame.Convert<Gray, byte>();
+            MCvAvgComp[][] facesDetectedNow = grayFace.DetectHaarCascade(faceDetected, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
+            foreach (MCvAvgComp f in facesDetectedNow[0])
+            {
+                result = Frame.Copy(f.rect).Convert<Gray, Byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                Frame.Draw(f.rect, new Bgr(Color.Green), 3);
+                if (trainingImages.ToArray().Length != 0)
+                {
+                    MCvTermCriteria termCriteria = new MCvTermCriteria(Count, 0.001);
+                    EigenObjectRecognizer recognizer = new EigenObjectRecognizer(trainingImages.ToArray(), labels.ToArray(), 1500, ref termCriteria);
+                    name = recognizer.Recognize(result);
+                    Frame.Draw(name, ref font, new Point(f.rect.X - 2, f.rect.Y - 2), new Bgr(Color.Red));
+                }
+                //Users[t - 1] = name;
+                Users.Add("");
+            }
+            cameraBox.Image = Frame;
+            names = "";
+            Users.Clear();
         }
     }
 }
